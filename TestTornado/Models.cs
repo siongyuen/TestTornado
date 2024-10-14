cc4e07f7ffaa5a7079d470d32cb07cf879f99061
siongyuencheahgoviewpointcom <169017212+siongyuencheahgoviewpointcom@users.noreply.github.com>
2024-10-14 11:21:44 +0800

    Split into Data Generator and Models

---
+++ TestTornado/Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTornado
+{
+    public class Customers
+    {
+        public List<Customer> customers { get; set; }
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Product> Products { get; set; }
+    }
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public bool OutOfStock { get; set; }
+    }
+}
--- TestTornado/Program.cs
+++ TestTornado/Program.cs
@@ -9,24 +9,7 @@ using System.Diagnostics;
 
 namespace TestTornado
 {
-    public class Customers
-    {
-        public List<Customer> customers { get; set; }
-    }
-    public class Customer
-    {
-        public string Name { get; set; }
-        public List<Product> Products { get; set; }
-    }
-    public class Product
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public decimal Discount { get; set; }
-        public bool OutOfStock { get; set; }
-    }
+   
 
  
     /// 
@@ -182,102 +165,14 @@ namespace TestTornado
                 templateName = TEMPLATE,
                 outputName = OUTPUT_FILE,
                 outputFormat = OUTPUT_FORMAT,
-                data = new
-                {
-                    customers = new List<Customer>
-            {
-                new Customer
-                {
-                    Name = "约翰亨利 John Henry",
-                    Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Name = "Laptop", Id = "P001", Quantity = 1, Price = 1000m, Discount = 100m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Mouse", Id = "P002", Quantity = 2, Price = 25m, Discount = 0m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Keyboard", Id = "P003", Quantity = 1, Price = 50m, Discount = 0m, OutOfStock = false
-                        },
-                         new Product
-                        {
-                            Name = "Laptop 2", Id = "P004", Quantity = 1, Price = 3000m, Discount = 10m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Mouse 2", Id = "P005", Quantity = 2, Price = 25m, Discount = 0m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Keyboard 2", Id = "P006", Quantity = 1, Price = 50m, Discount = 0m, OutOfStock = false
-                        }
-                    }
-                },
-                new Customer
-                {
-                    Name = "Jane Smith",
-                    Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Name = "Smartphone", Id = "P004", Quantity = 1, Price = 800m, Discount = 50m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Charger", Id = "P005", Quantity = 1, Price = 20m, Discount = 0m, OutOfStock = true
-                        },
-                             new Product
-                        {
-                            Name = "Smartphone 2", Id = "P006", Quantity = 1, Price = 800m, Discount = 50m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Charger 2", Id = "P005", Quantity = 1, Price = 20m, Discount = 0m, OutOfStock = true
-                        }
-
-                    }
-                },
-                new Customer
-                {
-                    Name = "Alice Johnson",
-                    Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Name = "Tablet", Id = "P006", Quantity = 1, Price = 300m, Discount = 25m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Tablet Case", Id = "P007", Quantity = 1, Price = 30m, Discount = 5m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Stylus Pen", Id = "P008", Quantity = 1, Price = 15m, Discount = 0m, OutOfStock = true
-                        },
-                            new Product
-                        {
-                            Name = "Tablet 2", Id = "P016", Quantity = 1, Price = 1500m, Discount = 10m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Tablet Case 2", Id = "P007", Quantity = 1, Price = 30m, Discount = 5m, OutOfStock = false
-                        },
-                        new Product
-                        {
-                            Name = "Stylus Pen 2", Id = "P008", Quantity = 1, Price = 15m, Discount = 0m, OutOfStock = true
-                        }
-                    }
-                }
-            }
-                }
+                data = DataGenerator.GetData()
             };
 
             return JsonSerializer.Serialize(requestObject, new JsonSerializerOptions { WriteIndented = true });
         }
+
+       
+
 
 
         private static async Task SaveToFileAsync(Stream content, string outputFilePath)
